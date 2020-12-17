24bab34af01d8e7d58cf34a0dceaf0a96dba8545
rohit <rohitsdec4@gmail.com>
2020-12-17 13:26:35 +0530

    Caesar Forward Backward variant is working

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -59,11 +59,73 @@ class Caesar:
             msg.append("".join(op))
        
         return " ".join(msg)
+#############################################
+class CaesarFB(Caesar):
+    front = None
+    back = None
+    p = None
+
+    def setParam(self, front, back, p):
+        if front is not None:
+            self.front = front
+        else:
+            print("Error: Invalid argument 1")
+        if back is not None:
+            self.back = back
+        else:
+            print("Error: Invalid argument 2") 
+        if p is not None:
+            self.p = p
+        else:
+            print("Error: Invalid argument 3")
+
+    def forward(self, portion, key=None):
+        op = []
+        if key is None:
+            key = self.front
+        for p in portion:
+            oldchar = ord(p)
+            if oldchar == 32:
+                op.append(p)
+            else:
+                newchar = self.shiftForward(oldchar,key=key) if oldchar > 96 else self.shiftForward(oldchar, False, key)
+                op.append(chr(newchar))            
+
+        return "".join(op)
+
+    def backward(self, portion, key=None):
+        op = []
+        if key is None:
+            key = self.back
+        for p in portion:
+            oldchar = ord(p)
+            if oldchar == 32:
+                op.append(p)
+            else:
+                newchar = self.shiftBackward(oldchar,key=key) if oldchar > 96 else self.shiftBackward(oldchar, False, key)
+                op.append(chr(newchar))            
+
+        return "".join(op)
+
+
+    def encrypt(self, message):
+        forward = self.forward(message[:self.p-1])
+        backward = self.backward(message[self.p-1:])
+        return (forward + backward)
+    
+    def decrypt(self, message):
+        forward = self.backward(message[:self.p-1],self.front)
+        backward = self.forward(message[self.p-1:],self.back)
+        return forward + backward
+
 
 if __name__ == '__main__':
     inp = input("Enter the text to be encrypted: ")
-    k = int(input("Enter the key: "))
-    cipher = Caesar(k)
-    enc = Caesar.encrypt(message=inp)
+    # k = int(input("Enter the key: "))
+    cipher = CaesarFB()
+    cipher.setParam(2,1,2)
+    enc = cipher.encrypt(message=inp)
     print(enc)
+    dec = cipher.decrypt(message=enc)
+    print(dec)
     # print(*output, sep=" ")
